0facdf6e68c0ed78781a675c0f639502cc98daa6
juandavid015 <juandavidgr1002@gmail.com>
2023-06-23 18:36:46 -0500

    Add&Set: Apollo client configuration + Auth&Error middlewares

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,18 +1,66 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, ApolloLink, from,} from '@apollo/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { onError } from "@apollo/client/link/error";
+//This must be on .env file
+const domain = 'dev-lmxqx7b47rlxp7f7.us.auth0.com';
+const clientId = 'D7ua33bZSRVkRI0GSoPRfqZoFWOz8Tau';
+const audience = 'https://xmanagement.com';
 
+const errorLink = onError(({ graphQLErrors, networkError}) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/' })
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+const token = localStorage.getItem('token');
+  operation.setContext({
+    headers: {
+      authorization: token ? `${token}` : "",
+    },
+  });
+  return forward(operation);
+});
+const typenameParseMiddleware = new ApolloLink((operation, forward) => {
+  // idea taked from https://github.com/apollographql/apollo-client/issues/1913
+  if (operation.variables) {
+    const removeTypenameFromVariables = (variables: any) => {
+      return JSON.parse(JSON.stringify(variables), (key, value) => {
+        return key === '__typename' ? undefined : value;
+      });
+    };
+    // eslint-disable-next-line no-param-reassign
+    operation.variables = removeTypenameFromVariables(operation.variables);
+  }
+  return forward(operation);
+});
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
+  link: from([ authMiddleware, errorLink, typenameParseMiddleware, httpLink ]),
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <Auth0Provider 
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{redirect_uri: window.location.origin, audience: audience}}
+      >
+        <App />
+      </Auth0Provider>
     </ApolloProvider>
   </React.StrictMode>,
 )
